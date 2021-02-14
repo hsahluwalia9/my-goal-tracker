8ef6e3c27f2eef9d9f2578240a54d02acc660c38
Harsimar Ahluwalia <harsimar.ahluwalia@gmail.com>
2021-02-14 10:17:11 -0500

    Managed to get server talking to db, however migrations are not generating for some reason

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,29 +1,24 @@
-const express = require('express');
-const cors = require('cors');
+import express from "express";
+import cors from "cors";
+import "reflect-metadata";
+import { createConnection, getConnection } from "typeorm";
+import { Goal } from "./entity/Goal";
+
+createConnection();
 
 const app = express();
 app.use(cors());
 const port = process.env.port || 5000;
 
-interface Goal {
-    title: string
+interface IGoal {
+  title: string;
 }
 
-app.get('/goals', (_req: any, res: { json: (arg0: Goal[]) => void; }) => {
-    const goals = [
-        {
-            title: "Exercise daily"
-        },
-        {
-            title: "Sleep 8 hours daily"
-        },
-        {
-            title: "Study software engineering"
-        }
-    ]
-    res.json(goals);
+app.get("/goals", async (_req: any, res: { json: (arg0: IGoal[]) => void }) => {
+  let goals = await getConnection().getRepository(Goal).find();
+  return res.json(goals);
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
